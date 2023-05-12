9d18af6ca8598eabae356730338261ce779b4e9c
Yamin <myamin335@gmail.com>
2023-05-12 18:06:21 +0200

    Initial Commit

---
+++ SwiftUINavigationSplitView/SwiftUINavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationSplitViewApp.swift
+//  SwiftUINavigationSplitView
+//
+//  Created by Md. Yamin on 12.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
